d009d830e72db8dec83927193dea335ef2c7fae9
kosmickizaborav <ninabogdanovic@yahoo.com>
2024-08-21 15:54:48 +0200

    reorganized downloaded censuses into folders by year

--- 00_download_drive.R
+++ 00_download_drive.R
@@ -3,21 +3,23 @@ library(here)
 library(googledrive)
 
 # login to google account
-# drive_auth()
+drive_auth("ninaupupina@gmail.com")
 
 
-if(!dir.exists(census_dir)){
-  
-  if(!dir.exists(here("Data"))) { here("Data") |> dir.create() }
-  
-  census_dir |> dir.create()
-}
 
 # 1 - Downloading census files from Google drive-------------------------------
 
 drive_dir <- "BBDD_ALEX_Ocells_Aiguamolls"
 
 census_dir <- here(here("Data", "Census_alex"))
+
+if(!dir.exists(census_dir)){
+  
+  if(!dir.exists(here("Data"))) { here("Data") |> dir.create() }
+  
+  census_dir |> dir.create()
+  
+}
 
 years <- c("2024")
 
@@ -38,27 +40,33 @@ drive_get(drive_dir) |>
   group_split(name) |> 
   map(~{
     
+    year <- .x$year
+    
+    if(!dir.exists(here(census_dir, year))) {
+      dir.create(here(census_dir, year))
+    }
+    
     drive_ls(.x, type = "xlsx") |> 
       as_tibble() |> 
-      mutate(year = .x$year)
+      mutate(year = .x$year) |> 
+      bind_rows() |> 
+      group_split(name) |> 
+      map(~{
+        
+        f_name <- .x$name |> 
+          str_remove(".xlsx") |> 
+          janitor::make_clean_names() |> 
+          str_replace_all(c("cens0" = "cens_0", "vila_sacra" = "vilasacra"))
+        
+        drive_download(
+          .x, 
+          path = here(census_dir, year, str_c(.x$year, "_", f_name, ".xlsx")), 
+          overwrite = T
+        )
+        
+      })
     
-  }) |> 
-  bind_rows() |> 
-  group_split(name) |> 
-  map(~{
-    
-    f_name <- .x$name |> 
-      str_remove(".xlsx") |> 
-      janitor::make_clean_names() |> 
-      str_replace_all(c("cens0" = "cens_0", "vila_sacra" = "vilasacra"))
-      
-    
-    drive_download(
-      .x, 
-      path = here("Data", "Census_alex", str_c(.x$year, "_", f_name, ".xlsx")), 
-      overwrite = T
-    )
-  })
+  }) 
 
 
 
