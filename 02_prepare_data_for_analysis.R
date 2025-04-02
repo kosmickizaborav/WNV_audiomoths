342ed55eea92dace6c571c3dbbc10284d6e2a0ac
Nina Bogdanovic <ninabogdanovic@yahoo.com>
2025-04-02 14:49:45 +0200

    updated the script "02_perepare_data_for_analysis" so that it is easier to read, and that it gathers all the data for one year in one folder, does not separate them by transect like before. changed sp_coratalet species so that they match birdlife names when possible.

--- 02_prepare_data_for_analysis.R
+++ 02_prepare_data_for_analysis.R
@@ -5,299 +5,368 @@
 
 # INFO --------------------------------------------------------------------
 
-#' **SECTION 1 - Standardizing census data for analysis**
-#' reformatting alex' tables to be easier for analysis. 
-#' the script is tailored so that it includes data from 2023 and 2024 and the 
-#' way it was formatted
+#' **SECTION 1 -  Extract transect information**
+#' from each transect files, extract info about the times and dates
+#' of transect as well as the ornitologist id. 
 #' 
-#' **SECTION 2 - Gathering results from BirdNet analysis**
-#' gathering all the BirdNet data available for the transect in one file
+#' **SECTION 2 - Prepare transect files 2024**
+#' reformatting alex' tables to be easier for analysis, 
+#' gather all information for that year in one file. 
+#' done for the two years separately because the data format is not identical
+#' 
+#' **SECTION 3 - Prepare transect files 2023**
+#' reformatting alex' tables to be easier for analysis, 
+#' gather all information for that year in one file. 
+#' done for the two years separately because the data format is not identical
+#' 
+#' **SECTION 2 - Gather irdNet results**
+#' gathering all the BirdNet data available for the year in one file
 #' the results were obtained running this:
 #' python3 /home/nina/BirdNET-Analyzer/analyze.py 
 #' --i /home/nina/Audiomoths/Files --o /home/nina/Audiomoths/Results 
 #' --lat 42.225039 --lon 3.092257
 
-
-
-
-# packages and directories ------------------------------------------------
+# 0 - Packages and directories --------------------------------------------
 
 library(tidyverse)
 library(here)
 library(readxl)
+library(janitor)
 source(here("00_functions.R"))
 
 # folder name where the data will be downloaded
-census_dir <- here("Data", "Census")
+data_dir <- here("Data")
+census_dir <- file.path(data_dir, "Census")
+birdnet_dir <- file.path(data_dir,"Birdnet")
+audiomoth_results_dir <- "/home/nina/Audiomoths/Results"
+
 years <- c(2023, 2024)
 
-birdnet_dir <- here("Data", "Birdnet")
-audiomoth_results <- "/home/nina/Audiomoths/Results"
+# 1 - Extract transect information --------------------------------------------
 
-# 1 - Standardizing census data for analysis ------------------------------
+data_list <- file.path(data_dir, "01_files_downloaded_from_drive.csv") |> 
+  read_csv(show_col_types = F) |> 
+  rename(original_file_name = name) |> 
+  select(contains("file_name"), transect, month, year) |> 
+  mutate(year_folder = file.path(census_dir, year))
 
-# census info columns
+
+# only for this year we have the calendar list
+calendar2024 <- data_list |> 
+  filter(file_name == "Calendari_censos.xlsx") |> 
+  mutate(file_path = file.path(year_folder, file_name)) |> 
+  pull(file_path) |> 
+  read_xlsx(n_max = 10) |> 
+  rename(month = 1) |> 
+  mutate(month = tolower(month)) |> 
+  rename_with(~str_remove(.x, "\\*"), everything()) |> 
+  pivot_longer(
+    cols = -month, 
+    names_to = "trans_official_name", 
+    values_to = "calendar_date"
+  ) |> 
+  mutate(
+    trans_official_name = str_replace_all(
+      str_remove_all(trans_official_name, " "), "[:punct:]", "_"
+    ), 
+    year = 2024, 
+    calendar_date = as_date(calendar_date)
+  )
+
+# information columns that we want to extract from the transect files
 info_cols <- c("data", "transecte", "horari_inici", "horari_final", "ornitoleg")  
+
+info_all <- data_list |> 
+  filter(!is.na(transect)) |> 
+  group_split(file_name) |> 
+  map(~{
+    
+    file_info <- .x
+    
+    trans_name <- file_info$transect
+    year_dir <- file_info$year_folder
+    
+    fin <- file_info$file_name
+    
+    # all the relevant info is in the first column
+    info <- file.path(year_dir, fin) |> 
+      read_xlsx(col_names = F) |> 
+      clean_names() |> 
+      select(1) 
+    
+    # getting row where the list of the species start
+    sp_row <- which(str_to_lower(as_vector(info)) == "especie")
+    
+    info <- info |> 
+      # first 5 rows have census info data
+      filter(row_number() < sp_row) |> 
+      filter(!is.na(x1)) |> 
+      # all info are in one column, separate them
+      separate_wider_delim(
+        x1, ":", names = c("var_name", "value"), 
+        too_many = "merge", 
+        too_few = "align_start"
+      ) |> 
+      # change some observed typos
+      mutate(
+        var_name = make_clean_names(var_name),
+        value = str_replace(value, "'", ":")
+      ) |> 
+      filter(var_name %in% info_cols) |>
+      #make columns for each info value
+      pivot_wider(names_from = var_name, values_from = value) |>
+      # remove extra spaces
+      mutate_all(str_squish) |>
+      mutate(
+        trans_official_name = trans_name, 
+        file_name = fin, 
+        sp_row = sp_row
+      ) 
+    
+  }) |> 
+  list_rbind() |> 
+  mutate(data = as_date(data, format = "%d/%m/%Y"))
+
+transect_info <- data_list |> 
+  filter(!is.na(transect)) |>
+  left_join(info_all, by = "file_name") |> 
+  left_join(calendar2024, by = c("trans_official_name", "month", "year")) |> 
+  select(-transecte, -trans_official_name) |> 
+  rename(start_time = horari_inici, end_time = horari_final) |> 
+  rename(
+    date_transect_file = data, 
+    date_calendar2024 = calendar_date
+  ) |> 
+  mutate(
+    date = if_else(
+      is.na(date_transect_file), date_calendar2024, date_transect_file
+    )
+  )
+
+rm(info_all, calendar2024, data_list)
+
+transect_info |> 
+  select(-year_folder, -sp_row) |> 
+  write_rds(file.path(census_dir, "02_transect_info.rds"))
+
+
+# 2 - Prepare transect files 2024----------------------------------------------
 
 # for renameing columns
 repl <- c("x4" = "s1", "x6" = "s2", "x8" = "s3", "x10" = "s4", "x12" = "s5", 
           "x14" = "s6")
 
+transect_info |> 
+  filter(year == 2024) |> 
+  select(year_folder, file_name) |> 
+  group_split(file_name) |> 
+  map(~{
+    
+    fin <- .x$file_name
+    fin_path <- file.path(.x$year_folder, fin)
+    
+    # loading transect data
+    fin_path |> 
+      # skip info columns
+      read_xlsx(skip = 5) |> 
+      clean_names() |>
+      rename(total_radius = x15, total_transect = x16) |>
+      select(-starts_with("total")) |>
+      filter(row_number() != 1) |>
+      # to remove extra NA fields that sometimes occur in the excel
+      # e.g. they have banda value but nothing else
+      # also so that I can do species below
+      filter(row_number() <= max(which(!is.na(especie))+2)) |>
+      # for each species there are 3 rows,
+      # indicating the distance of detection,
+      # but they are not explicitly written
+      mutate(
+        species = rep(especie[which(!is.na(especie))], each = 3)
+      ) |>
+      # these are the columns for the sector
+      pivot_longer(
+        cols = 3:14, names_to = "sector", values_to = "count"
+      ) |>
+      mutate(
+        count = as.numeric(count),
+        # if it contains s, it is mascla, otherwise the column name is x_,
+        # it's a by_product of how alex's table is structured
+        bird_cat = if_else(str_detect(sector, "s"), "mascla", "altra"),
+        # renaming x_ with the adequate segment names
+        sector = str_replace_all(sector,  repl)
+      ) |>
+      summarise(
+        total = sum(count, na.rm = T),
+        .by = any_of(c("species", "sector", "banda"))
+      ) |>
+      filter(total > 0) |> 
+      mutate(file_name = fin)
+    
+  }) |> 
+  list_rbind() |>
+  left_join(
+    transect_info |> select(file_name, date, transect), by = "file_name"
+  ) |> 
+  mutate(
+    species_corr = str_remove_all(species, "[0-9[:punct:]]") |> str_squish()
+  ) |> 
+  check_birdlife(species_corr) |> 
+  mutate(
+    birdlife_name = if_else(
+      str_detect(species_corr, " sp$") | str_count(species_corr, " ") > 1, 
+      NA, 
+      birdlife_name
+    )
+  ) |> 
+  select(-species_corr) |> 
+  write_rds(file.path(census_dir, "2024_complete_data.rds"))
 
-years |> 
-  walk(~{
+
+# 2 - Prepare transect files 2023----------------------------------------------
+
+
+transect_info |> 
+  filter(year == 2023) |> 
+  select(year_folder, file_name, sp_row) |> 
+  group_split(file_name) |> 
+  map(~{
     
-    year <- .x 
+    fin <- .x$file_name
+    fin_path <- file.path(.x$year_folder, fin)
+    sp_row <- .x$sp_row
     
-    trans_dirs <- here(census_dir, year) |> 
-      list.dirs() 
-    
-    if(year == 2024){
-      
-      calendar <- here(census_dir, year) |> 
-        list.files(pattern = "calendar", full.names = T) |> 
-        read_xlsx(n_max = 10) |> 
-        rename(month = 1) |> 
-        mutate(month = tolower(month)) |> 
-        rename_with(~str_remove(.x, "\\*"), everything()) |> 
-        pivot_longer(
-          cols = -month, 
-          names_to = "trans_official_name", 
-          values_to = "calendar_date"
+    # loading transect data
+    fin_path |> 
+      # skip info columns
+      read_xlsx(skip = sp_row-1) |>
+      clean_names() |>
+      select(-starts_with("total")) |>
+      mutate(species = str_squish(especie)) |>
+      # to eliminate the columns at the end that have total value per
+      # sector
+      filter(!is.na(species) & str_to_lower(species) != "total") |>
+      # in some counts there was labels like 30+, 40+
+      mutate(
+        across(
+          any_of(matches("^s[1-9]$")),
+          ~as.numeric(str_remove_all(.x, "[^0-9]"))
         )
-      
-    }
+      ) |>
+      pivot_longer(
+        cols = any_of(matches("^s[1-9]$")),
+        names_to = "sector",
+        values_to = "count"
+      ) |>
+      mutate(count = as.numeric(count)) |> 
+      summarise(
+        total = sum(count, na.rm = T),
+        .by = c("species", "sector")
+      ) |>
+      filter(total > 0) |> 
+      mutate(file_name = fin)
     
-    # [-1] to remove the main directory (here(census_dir, year))
-    trans_dirs[-1] |> 
-      walk(~{
-        
-        trans_dir <- .x 
-        
-        print(trans_dir)
-        
-        # take the folder name as the transect name in order to standardize it 
-        # easier
-        trans_name <- str_split_i(trans_dir, "/", -1)
-        
-        trans_dir |> 
-          list.files() |> 
-          map(~{
-            
-            fname <- .x
-            
-            print(fname)
-            
-            keys <- fname |> 
-              str_remove(".xlsx") |> 
-              str_replace_all("_", "|") |> 
-              reduce(str_c)
-            
-            info <- here(trans_dir, fname) |> 
-              read_xlsx(col_names = F) |> 
-              janitor::clean_names() |> 
-              select(1)
-            
-            # getting row where the list of the species start
-            sp_row <- which(str_to_lower(as_vector(info)) == "especie")
-            
-            info <- info |> 
-              # first 5 rows have census info data
-              filter(row_number() < sp_row) |> 
-              filter(!is.na(x1)) |> 
-              # all info are in one column, separate them
-              separate_wider_delim(
-                x1, ":", names = c("var_name", "value"), 
-                too_many = "merge", 
-                too_few = "align_start"
-              ) |> 
-              # change some observed typos
-              mutate(
-                var_name = str_replace(var_name, " ", "_") |> 
-                  str_to_lower(),
-                value = str_replace(value, "'", ":")
-              ) |> 
-              filter(var_name %in% info_cols) |>
-              #make columns for each info value
-              pivot_wider(names_from = var_name, values_from = value) |>
-              # remove extra spaces
-              mutate_all(str_squish) |>
-              mutate(trans_official_name = trans_name)
-
-            
-            if(year == 2024){
-
-              c_date <- calendar |>
-                filter(str_detect(month, keys)) |>
-                filter(str_detect(tolower(trans_official_name), keys))
-
-              info <- info |>
-                left_join(c_date, by = "trans_official_name")
-
-              # loading transect data
-              trans_df <- here(trans_dir, fname)|>
-                # skip info columns
-                read_xlsx(skip = 5) |>
-                janitor::clean_names() |>
-                rename(total_radius = x15, total_transect = x16) |>
-                select(-starts_with("total")) |>
-                filter(row_number() != 1) |>
-                # to remove extra NA fields that sometimes occur in the excel
-                # e.g. they have banda value but nothing else
-                # also so that I can do species below
-                filter(row_number() <= max(which(!is.na(especie))+2)) |>
-                # for each species there are 3 rows,
-                # indicating the distance of detection,
-                # but they are not explicitly written
-                mutate(
-                  species = rep(especie[which(!is.na(especie))], each = 3)
-                ) |>
-                # these are the columns for the sector
-                pivot_longer(
-                  cols = 3:14, names_to = "sector", values_to = "count"
-                ) |>
-                mutate(
-                  count = as.numeric(count),
-                  # if it contains s, it is mascla, otherwise the column name is x_,
-                  # it's a by_product of how alex's table is structured
-                  bird_cat = if_else(str_detect(sector, "s"), "mascla", "altra"),
-                  # renaming x_ with the adequate segment names
-                  sector = str_replace_all(sector,  repl)
-                ) |>
-                summarise(
-                  total = sum(count, na.rm = T),
-                  .by = any_of(c("species", "sector", "banda"))
-                ) |>
-                mutate(
-                  trans_official_name = trans_name,
-                  file = fname
-                ) |>
-                left_join(info, by = "trans_official_name")
-
-            }
-
-
-            if(year == 2023){
-
-              # loading transect data
-              trans_df <- here(trans_dir, fname)|>
-                # skip info columns
-                read_xlsx(skip = sp_row-1) |>
-                janitor::clean_names() |>
-                select(-starts_with("total")) |>
-                mutate(species = str_squish(especie)) |>
-                # to eliminate the columns at the end that have total value per
-                # sector
-                filter(!is.na(species) & str_to_lower(species) != "total") |>
-                # in some counts there was labels like 30+, 40+
-                mutate(
-                  across(
-                    any_of(matches("^s[1-9]$")),
-                    ~as.numeric(str_remove_all(.x, "[^0-9]"))
-                  )
-                ) |>
-                pivot_longer(
-                  cols = any_of(matches("^s[1-9]$")),
-                  names_to = "sector",
-                  values_to = "count"
-                ) |>
-                mutate(count = as.numeric(count)) |>
-                summarise(
-                  total = sum(count, na.rm = T),
-                  .by = c("species", "sector")
-                ) |>
-                mutate(
-                  trans_official_name = trans_name,
-                  file = fname
-                ) |>
-                left_join(info, by = "trans_official_name")
-            }
-            
-            trans_df
-
-          }) |> 
-          bind_rows() |> 
-          mutate(
-            species = str_remove_all(species, "[0-9[:punct:]]") |> str_squish()
-          ) |> 
-          check_birdlife(species) |> 
-          write_csv(
-            here(
-              census_dir,
-              str_c(
-                year, "_", str_remove(trans_name, " "), "_complete_data.csv")
-              )
-            )
-      }
-      )
-    print(paste(year, "DONE!"))
-  }
-  )
-
+  }) |> 
+  list_rbind() |> 
+  left_join(
+    transect_info |> select(file_name, date, transect), by = "file_name"
+  ) |> 
+  mutate(
+    species_corr = str_remove_all(species, "[0-9[:punct:]]") |> str_squish()
+  ) |> 
+  check_birdlife(species_corr) |> 
+  mutate(
+    birdlife_name = if_else(
+      str_detect(species_corr, " sp$") | str_count(species_corr, " ") > 1, 
+      NA, 
+      birdlife_name
+    )
+  ) |> 
+  select(-species_corr) |> 
+  write_rds(file.path(census_dir, "2023_complete_data.rds"))
 
 # 2 - Gathering BirdNet results -----------------------------------------------
 
-# because there is no data for that year
-years <- years[years != 2023]
+sp_cortalet <- file.path(data_dir, "species_cortalet.txt") |> 
+  read_delim(
+    col_names = c("birdnet_scientific_name", "birdnet_common_name"), 
+    delim = "_", 
+    show_col_types = F
+  ) |> 
+  check_birdlife(birdnet_scientific_name) |> 
+  mutate(
+    birdlife_name = if_else(
+      name_type == "synonyms", alternative_for_synonym, birdlife_name
+    )
+  ) |> 
+  select(-alternative_for_synonym) |>
+  mutate(
+    scientific_name = if_else(
+      is.na(birdlife_name), birdnet_scientific_name, birdlife_name
+    )
+  )
 
+sp_cortalet |> 
+  write_csv(file.path(birdnet_dir, "02_species_cortalet_to_birdlife.csv"))
+
+sp_cortalet <- sp_cortalet |> 
+  rename(common_name = birdnet_common_name) |> 
+  select(scientific_name, common_name) 
+            
 audiomoths <- here(birdnet_dir, "audiomoth_field_data.xlsx")|> 
   read_xlsx() |> 
-  janitor::clean_names() |> 
+  clean_names() |> 
   filter(!is.na(data_id)) |>
-  rename(tname_alex = transect_name_alex) |> 
-  mutate(year = year(start_date)) |> 
-  select(year, tname_alex, sector, data_id)
+  rename(transect = transect_name_alex) |> 
+  mutate(
+    year = year(start_date), 
+    transect = str_replace_all(
+      str_remove_all(transect, " "), "[:punct:]", "_"
+    )
+  ) |> 
+  select(year, transect, sector, data_id) 
 
 audiomoths |> 
-  group_split(tname_alex, year) |> 
+  group_split(year) |> 
   map(~{
     
-    transect_name <- unique(.x$tname_alex)
-    year <- unique(.x$year)
+    year_data <- .x
     
-    print(str_c(year, transect_name, "STARTED!", sep = " "))
+    year <- unique(year_data$year)
     
-    .x$data_id |> 
+    year_data |> 
+      group_split(data_id) |> 
       map(~{
         
-        data_id <- .x
-        print(data_id)
+        data_id <- .x$data_id
         
-        file.path(audiomoth_results, data_id) |>  
+        print(paste(year, data_id, "processing!"))
+        
+        file.path(audiomoth_results_dir, data_id) |>  
           # listing sub-folders for every day
           list.files(full.names = T) |> 
           # list the files in each subfolder and load them
           map(~list.files(.x, full.names = T)) |> 
           map(~read_tsv(.x, show_col_types = F)) |>
-          bind_rows() |> 
-          janitor::clean_names() |> 
+          list_rbind() |> 
+          clean_names() |> 
           mutate(
             data_id = data_id,
             # getting the .WAV file name
             recording = str_split_i(begin_path, "/", -1), 
             datetime = as_datetime(
               str_remove(recording, ".WAV"), format = "%Y%m%d_%H%M%S"
-            ), 
-            transect_name = transect_name
-          )
+            )
+          ) |> 
+          filter(species_code != "nocall") |> 
+          left_join(year_data, by = "data_id")
         
-        }
-      ) |> 
-      bind_rows() |> 
-      write_csv(
-        here(
-          birdnet_dir,
-          str_c(
-          year, "_", transect_name, "_complete_data.csv"
-          )
-        )
+      }) |> 
+      list_rbind() |> 
+      left_join(sp_cortalet, by = "common_name") |> 
+      write_rds(
+        file.path(birdnet_dir, str_c(year, "_complete_data.rds"))
       )
     
-    print(str_c(year, transect_name, "DONE!", sep = " "))
+    print(paste(year, "DONE!"))
     
-  }, 
-  .progress = T
-  )
-
-
-
+  })
