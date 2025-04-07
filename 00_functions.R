20f70de490c83dfe5bf05cf729f1d439689f80ac
Nina Bogdanovic <ninabogdanovic@yahoo.com>
2025-04-07 15:18:27 +0200

    finished birdlife checklist function, added automatic check for birdnet names.

--- 00_functions.R
+++ 00_functions.R
@@ -4,10 +4,9 @@ library(here)
 library(stringdist)
 
 
-# 1 match_to_birdlife --------------------------------------------------------
+# 1 - FUNCTION: match_names -----------------------------------------------
 
-# 1.0. small function for matching scientific names in case of typos --------
-
+# small function for matching scientific names in case of typos 
 match_names <- function(
     species, 
     scientific_names, 
@@ -29,16 +28,20 @@ match_names <- function(
 }
 
 
-# 1.1. full function for checking names -----------------------------------
 
-# used for testing
-# species_name <- bird_db$scientific_name
+# FUNCTION: check_birdlife ------------------------------------------------
 
 
-
-check_birdlife <- function(
-    df, species_name, max_d = 3, synonyms_to_sci_names = F
-    ) {
+check_birdlife <- function(df, species_name, max_d = 3, birdnet_check = T) {
+  
+  # obtained from BirdNet Analyser files in section Data
+  birdnet_sp <- here("BirdNET_GLOBAL_6K_V2.4_Labels.txt") |> 
+    read_delim(
+      col_names = c("scientific_name", "common_name"), 
+      delim = "_", 
+      show_col_types = F
+    ) |> 
+    pull(scientific_name)
   
   # downloaded from: 
   # https://datazone.birdlife.org/about-our-science/taxonomy
@@ -48,38 +51,118 @@ check_birdlife <- function(
     read_xlsx(skip = 2) |> 
     # take only the species level, not subspecies
     janitor::clean_names() |> 
-    filter(!is.na(subsp_seq)) |> 
-    select(scientific_name, synonyms, family_name) |> 
-    # remove genus in parenthesis if provided
+    rename(sp_status = x2024_iucn_red_list_category) |> 
+    select(seq, scientific_name, synonyms, family_name, sp_status) |> 
+    filter(!is.na(seq)) |> 
+    distinct(seq, .keep_all = T) |> 
+    # remove information in parenthesis is added to the synonyms column
     mutate(synonyms = str_remove_all(synonyms, "\\([^\\)]*\\)")) |> 
     # change all the punctuation to ";"
     mutate(synonyms = str_replace_all(synonyms, "[:punct:]", ";")) |> 
     # extract synonyms one per row
     separate_longer_delim(synonyms, delim = ";") |> 
-    # clean extra white spaces
-    mutate(synonyms = str_squish(synonyms)) |>
-    # eliminate duplicates and repeated name as species and synonyms
-    distinct(scientific_name, synonyms, family_name) |> 
-    filter(scientific_name != synonyms | is.na(synonyms))  |> 
+    # remove labels for variants and subspecies
+    # remove synonyms that are not a full species name but just an epitaph or
+    # genus, it's the product of previous steps
+    mutate(synonyms = str_remove_all(synonyms, "\\s(sp$|var$|ssp$)")) |>
+    # making sure there is no extra white spaces
+    mutate(across(everything(), str_squish)) |> 
+    mutate(
+      # removing the synonyms that are just the epitaph or genus name
+      # or the ones that are identical to the scientific name
+      synonyms = case_when(
+        str_count(synonyms, " ") !=1 ~ NA,
+        scientific_name == synonyms ~ NA,
+        .default = synonyms
+      ), 
+      # change the status to NR - not recognized and R - regonized
+      sp_status = if_else(sp_status == "NR", "NR", "R")
+    ) |> 
+    distinct(scientific_name, synonyms, family_name, sp_status, .keep_all = T) |> 
+    mutate(
+      # checked that they don't have any synonyms, so I just manually added them
+      synonyms = case_when(
+        scientific_name == "Myiopsitta monachus" ~ "Psittacula monachus",
+        scientific_name == "Cyanistes caeruleus" ~ "Parus caeruleus",
+        scientific_name == "Larus melanocephalus" ~ "Ichthyaetus melanocephalus",
+        scientific_name == "Larus genei" ~ "Chroicocephalus genei",
+        scientific_name == "Larus audouinii" ~ "Ichthyaetus audouinii",
+        scientific_name == "Curruca melanocephala" ~ "Sylvia melanocephala", 
+        scientific_name == "Curruca hortensis" ~ "Sylvia hortensis" ,
+        .default = synonyms
+      )
+    ) |> 
+    # check how many times the species appears, 
+    # whether it is both listed as recognized and not
+    # how many distinct synonyms it has
+    mutate(
+      n_sci = n(),
+      both_r_nr = sum(c("R", "NR") %in% sp_status) == 2, 
+      dist_syn = n_distinct(synonyms, na.rm = T),
+      .by = scientific_name
+    ) |> 
+    # check unique combos
+    mutate(n_pair = n(), .by = c(scientific_name, synonyms)) |> 
+    mutate(
+      sci_in_syn = scientific_name %in% unique(synonyms), 
+      syn_in_sci = synonyms %in% unique(scientific_name)
+    ) |> 
+    mutate(
+      sci_name = case_when(
+        # if scientific name is both listed as recognized and not recognized, 
+        # remove the non recognized if it has no additional synonyms
+        n_sci > 1 & both_r_nr & sp_status == "NR" & is.na(synonyms) ~ NA,
+        # if it does have synonyms, make sure they are identical as in recognized
+        n_sci > 1 & both_r_nr & sp_status == "NR" & n_pair > 1 ~ NA,
+        # if the scientific name is duplicated but one of the entries doesn't
+        # have synonym provided, remove it
+        n_sci > 1 & dist_syn > 0 & is.na(synonyms) ~ NA, 
+        # if the scientific name is provided also found in the synonyms, but
+        # it's listed as not recognized, remove it if there is no synonym provided
+        sci_in_syn & sp_status == "NR" & is.na(synonyms) ~ NA,
+        .default = scientific_name
+      )
+    ) |> 
+    filter(!is.na(sci_name)) |> 
+    mutate(
+      sp_status = if_else(sp_status == "NR" & both_r_nr == T, "R", sp_status),
+      syn_in_sci = synonyms %in% unique(scientific_name), 
+      synonyms = if_else(syn_in_sci, NA, synonyms)
+    ) |> 
+    filter(
+      !(n() > 1 & n_distinct(synonyms, na.rm = T) > 0 & is.na(synonyms)), 
+      .by = scientific_name
+    ) |> 
+    select(scientific_name, synonyms, family_name, sp_status) |>
     # assign a id number just for easier organization
-    mutate(sp_id = 1:n())
+    mutate(sp_id = 1:n()) |> 
+    rename(synonym = synonyms) |> 
+    mutate(
+      birdnet_name = case_when(
+        scientific_name %in% birdnet_sp ~ scientific_name,
+        synonym %in% birdnet_sp ~ synonym,
+        .default = NA
+      )
+    )
   
   # extract all the names and synonyms in one table
   bln <- birdlife |> 
     pivot_longer(
-      cols = c(scientific_name, synonyms), 
+      cols = c(scientific_name, synonym), 
       names_to = "name_type", 
       values_to = "sci_name", 
       values_drop_na = T
-    ) |> 
-    filter(str_count(sci_name, " ") == 1) |> 
+    ) |>  
+    # unique scientific names and synonyms
     distinct(sci_name, name_type, .keep_all = T) |> 
-    # if a scientific name is also found in synonyms keep the scientific name
-    filter(
-      n_distinct(name_type) == 1 |
-      n_distinct(name_type) == 2 & name_type == "scientific_name", 
-      .by = sci_name
+    mutate(
+      name_type = if_else(
+        sp_status == "R", 
+        paste("recognized" , str_replace(name_type, "_", " ")), 
+        paste("not recognized", str_replace(name_type, "_", " "))
+      )
     ) |> 
+    # if a scientific name is also found in synonyms keep the scientific name
     # extract genus and species epithaf
     mutate(
       gen = str_split_i(sci_name, " ", 1), 
@@ -88,18 +171,10 @@ check_birdlife <- function(
   
   # extract genus and family names
   bln_gens <- bln |> 
-    distinct(gen, family_name, name_type) |> 
-    # if the genus is duplicated and the same type take the first one, 
-    # otherwise take the one that is labeled as scientific_name
-    filter(
-      n_distinct(name_type) == 1 & n() == 1 |
-        n_distinct(name_type) == 2 & name_type == "scientific_name", 
-      .by = gen
-    ) |> 
-    select(-name_type) |> 
-    # if the genuses are still duplicated keep the family that is closest to
-    # the genus name
-    filter(!duplicated(gen) | str_detect(gen, family_name)) 
+    distinct(gen, family_name) |> 
+    mutate(str_sim = stringsim(gen, family_name)) |> 
+    filter(n() == 1 | str_sim == max(str_sim), .by = gen) |> 
+    distinct(gen, family_name)
   
   # copy the species name column from the original data for easier manipulation
   df <- df |> 
@@ -114,7 +189,7 @@ check_birdlife <- function(
       by = c("spn" = "sci_name")
     )
   
-  # correct the genus and then correct the epithaph of the species within
+  # correct the genus and then correct the epitaph of the species within
   # that genus
   genus_correct <- correct |> 
     filter(is.na(sp_id)) |> 
@@ -192,7 +267,7 @@ check_birdlife <- function(
     mutate(
       alternative_for_synonym = ifelse(
         str_detect(name_type, "synonym"), 
-        birdlife$scientific_name[match(birdlife_name, birdlife$synonyms)], 
+        birdlife$scientific_name[match(birdlife_name, birdlife$synonym)], 
         NA
         )
     ) |>
@@ -202,19 +277,23 @@ check_birdlife <- function(
     left_join(out, by = "spn") |> 
     select(-spn)
   
-  # if we want to change the synonyms direclty to the accepted scientific names
-  if(synonyms_to_sci_names) {
+  df <- df |> 
+    mutate(
+      birdlife_name = if_else(
+        str_detect(name_type, "synonym"), 
+        alternative_for_synonym, 
+        birdlife_name
+      ) 
+    ) |> 
+    select(-alternative_for_synonym)
+  
+  if(birdnet_check){
     
     df <- df |> 
       mutate(
-        birdlife_name = if_else(
-          str_detect(name_type, "synonym"), 
-          alternative_for_synonym, 
-          birdlife_name
-        ) 
-      ) |> 
-      select(-alternative_to_synonym)
-          
+        birdnet_name = birdlife$birdnet_name[match(birdlife_name, birdlife$scientific_name)], 
+      )
+    
   }
   
   return(df)
